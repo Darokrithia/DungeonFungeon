366ea02dcaff099ecc6a9996ed2b35f82cca6df7
Darokrithia <dieltabin@gmail.com>
2017-07-30 14:47:26 -0600

    Trying to get js working...

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-int[][] roooms;
+function aButtonClick(){
+	document.getElementByID("nav").style.display = "initial";
+	document.getElementByID("choice").style.display = "none";
+}
+
+function northButtonClick(){
+	document.getElementByID("choice").style.display = "initial";
+	document.getElementByID("nav").style.display = "none";
+}
