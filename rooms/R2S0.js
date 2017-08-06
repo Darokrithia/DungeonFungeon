50fb1af2e782f6d6025ccab88c72907447b847c5
Darokrithia <dieltabin@gmail.com>
2017-08-05 23:30:57 -0600

    Tests

--- rooms/R2S0.js
+++ rooms/R2S0.js
@@ -2,4 +2,4 @@ var roomDsc = "This is Room 2, its just a test right now";
 var aOptn = "okay, lets go";
 var bOptn = "okay, lets go";
 var cOptn = "okay, lets go";
-var dOptn = makeRoomString(dungeonArray[x][y]);
+var dOptn = (makeRoomString(dungeonArray[x][y]) + " " + x + " " + y);
